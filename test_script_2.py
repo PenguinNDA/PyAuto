001c46a13d9e581003f5ef385ec339d843c98dd0
PenguinNDA <snejnyj2015@yandex.ru>
2022-06-22 15:00:43 +0300

    Little PEP fixes and first lesson from p2

--- test_script_2.py
+++ test_script_2.py
@@ -2,14 +2,15 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
+browser = webdriver.Chrome(ChromeDriverManager().install())
 link = "http://suninjuly.github.io/simple_form_find_task.html"
 
 try:
-    browser = webdriver.Chrome(ChromeDriverManager().install())
+
     browser.get(link)
     button = browser.find_element(By.ID, "submit_button")
     button.click()
 
 finally:
-    # закрываем браузер после всех манипуляций
+    # Close browser after all manipulation
     browser.quit()
